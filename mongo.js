1d7f07884a53198a7ea16953560e9efd99da0c76
Nadar-Suresh-Kumar <sureshkumarnadar1701@gmail.com>
2024-06-03 17:54:17 +0530

    h

---
+++ mongo.js
@@ -0,0 +1,31 @@
+require('dotenv').config()
+const mongoose=require("mongoose")
+
+
+mongoose.connect(process.env.MONG0_URL)
+.then(()=>{
+    console.log('mongoose connected');
+})
+.catch((e)=>{
+    console.log('failed');
+})
+
+const logInSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:false
+    },
+    password:{
+        type:String,
+        required:false
+    },
+    scored:{
+        type:Number,
+        required:false
+    }
+ 
+})
+
+const LogInCollection=new mongoose.model('LogInCollection',logInSchema)
+
+module.exports=LogInCollection
